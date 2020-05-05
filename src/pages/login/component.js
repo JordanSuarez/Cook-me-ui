dbcd10ef357daa2aab5c0663162987f803ffa33c
reslene <reslene.bahloul@kicklox.com>
2020-05-05 14:12:05 +0200

    feat: wip for FormSection + Login

--- src/pages/login/component.js
+++ src/pages/login/component.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField'
 
 function Login() {
-  return <div> Login</div>
+  // TODO handle data when submit button
+
+  return (
+    <div>
+      {/* TODO props : login(string) et children (node) */}
+      {/*<FormSection title="Login">*/}
+      <TextField required id="login" label="Login" variant="outlined" />
+      <TextField required id="password" label="Password" type="password" variant="outlined" autoComplete="current-password" />
+      {/* add submit button */}
+      {/*</FormSection>*/}
+    </div>
+  )
 }
 
 Login.propTypes = {}
