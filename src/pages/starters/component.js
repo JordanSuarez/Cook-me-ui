235fe1618e8b9343b8145f7589a9e24eef849d3c
reslene <reslene.bahloul@kicklox.com>
2020-05-13 13:16:55 +0200

    feat: useState to store recipes from api call + render List component from data

--- src/pages/starters/component.js
+++ src/pages/starters/component.js
@@ -1,22 +1,31 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 
-import {ALL} from '../../common/constants/resources_type'
+import {BY_RECIPE_TYPE} from '../../common/constants/resources_type'
 import {callApi} from 'common/helpers/repository'
-import {GET} from '../../common/constants/methods'
-import {getUrl} from '../../common/helpers/urlHandler'
+import {GET} from 'common/constants/methods'
+import {getEndpoint} from '../../common/helpers/urlHandler'
 import {RECIPES} from '../../common/constants/resources'
+import List from 'common/components/list'
 
 function Starters() {
+  const [recipes, setRecipes] = useState([])
+
   useEffect(() => {
-    const url = getUrl(RECIPES, GET, ALL)
+    const url = getEndpoint(RECIPES, GET, BY_RECIPE_TYPE, 61)
 
     callApi(url, GET)
-      // eslint-disable-next-line no-empty-pattern
-      .then(({}) => {})
+      .then(({data}) => {
+        setRecipes(data)
+      })
       .catch(() => {})
   }, [])
 
-  return <div>Starters List</div>
+  return (
+    <div>
+      Starters List
+      <List items={recipes} />
+    </div>
+  )
 }
 
 export default Starters
