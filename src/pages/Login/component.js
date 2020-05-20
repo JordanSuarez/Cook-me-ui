6d04f266aca2837463fd95baf52c08867a99f066
reslene <reslene.bahloul@kicklox.com>
2020-05-20 11:32:01 +0200

    improvement: add Container/Hidden component from Material UI, on Login page for responsive mobile

--- src/pages/Login/component.js
+++ src/pages/Login/component.js
@@ -1,19 +1,18 @@
 import React from 'react'
 
-import {Avatar, Button, Grid, Paper, Typography} from '@material-ui/core'
+import {Avatar, Button, Container, Grid, Hidden, Paper, Typography} from '@material-ui/core'
 import {useHistory} from 'react-router-dom'
-
-import LoginIcon from '@material-ui/icons/LockOutlined'
-
-import {TextField} from 'mui-rff'
-
-import {useTranslation} from 'react-i18next'
 
 import {Form} from 'react-final-form'
 
-import {classes as classesProps} from 'common/props'
+import {TextField} from 'mui-rff'
+import {useTranslation} from 'react-i18next'
+import LoginIcon from '@material-ui/icons/LockOutlined'
+
 import {getHomeRoute} from 'common/routing/routesResolver'
 import {handleLogin} from 'common/helpers/repository'
+
+import {classes as classesProps} from 'common/props'
 
 function Login({classes}) {
   const history = useHistory()
@@ -25,54 +24,58 @@ function Login({classes}) {
   }
 
   return (
-    <Grid container spacing={3} className={classes.root}>
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6}>
-        <div className={classes.paper}>
-          <Avatar className={classes.icon}>
-            <LoginIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            {t('loginPage.title')}
-          </Typography>
-          <Form
-            onSubmit={onSubmit}
-            // validate={(values) => {
-            // test if username and password are empty
-            // if empty setErrorDisplay(true)
-            // }}
-            render={({handleSubmit}) => (
-              <form onSubmit={handleSubmit} noValidate className={classes.form}>
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  label={t('loginPage.form.label.username')}
-                  name="username"
-                  autoFocus
-                  // add props 'error' if and only if state attribute 'errorDisplay' is true
-                />
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  label={t('loginPage.form.label.password')}
-                  name="password"
-                  autoComplete="current-password"
-                  type="password"
-                  // add props 'error' if and only if state attribute 'errorDisplay' is true
-                />
-                <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
-                  {t('loginPage.form.button')}
-                </Button>
-              </form>
-            )}
-          />
-        </div>
+    <Container fixed>
+      <Grid container spacing={3} className={classes.root}>
+        <Hidden xsDown>
+          <Grid item xs={12} sm={4} md={7} className={classes.image} />
+        </Hidden>
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6}>
+          <div className={classes.paper}>
+            <Avatar className={classes.icon}>
+              <LoginIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              {t('loginPage.title')}
+            </Typography>
+            <Form
+              onSubmit={onSubmit}
+              // validate={(values) => {
+              // test if username and password are empty
+              // if empty setErrorDisplay(true)
+              // }}
+              render={({handleSubmit}) => (
+                <form onSubmit={handleSubmit} noValidate className={classes.form}>
+                  <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    label={t('loginPage.form.label.username')}
+                    name="username"
+                    autoFocus
+                    // add props 'error' if and only if state attribute 'errorDisplay' is true
+                  />
+                  <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    label={t('loginPage.form.label.password')}
+                    name="password"
+                    autoComplete="current-password"
+                    type="password"
+                    // add props 'error' if and only if state attribute 'errorDisplay' is true
+                  />
+                  <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+                    {t('loginPage.form.button')}
+                  </Button>
+                </form>
+              )}
+            />
+          </div>
+        </Grid>
       </Grid>
-    </Grid>
+    </Container>
   )
 }
 
