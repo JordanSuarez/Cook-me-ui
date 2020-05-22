ba00276e84e9c74ad1151095b96c828425bc7f24
reslene <reslene.bahloul@kicklox.com>
2020-05-22 11:19:33 +0200

    feat: create ListTable component for Starters/Dish/Deserts pages
    add new data on .json for i18N. TODO, add button to switch with ListCard and ListTable component in
    Starters/Dish/Deserts pages

---
+++ src/common/components/ListTable/component.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import {any, arrayOf} from 'prop-types'
+
+import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@material-ui/core'
+
+import {useTranslation} from 'react-i18next'
+
+import {classes as classesProps} from '../../props'
+
+function ListTable({items, classes}) {
+  const {t} = useTranslation('starters')
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="left">{t('recipeListTable.name')}</TableCell>
+            <TableCell align="left">{t('recipeListTable.instruction')}</TableCell>
+            <TableCell align="right">{t('recipeListTable.preparationTime')}</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {items.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell align="left">{item.name}</TableCell>
+              <TableCell align="left">{item.instruction}</TableCell>
+              <TableCell align="right">{item.preparationTime}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
+
+ListTable.propTypes = {
+  items: arrayOf(any),
+  ...classesProps,
+}
+
+ListTable.defaultProps = {
+  items: [],
+}
+
+export default ListTable
