c9d11032b2155aa65bc688331e2539e17d2735b7
Jordan MacBook <suarez.jordan@orange.fr>
2020-09-21 10:47:43 +0200

    Style: add some style for navBar

--- src/common/components/NavBar/component.js
+++ src/common/components/NavBar/component.js
@@ -2,16 +2,19 @@ import React from 'react'
 
 import {Button, Grid} from '@material-ui/core'
 import {string} from 'prop-types'
-import {useHistory} from 'react-router-dom'
+import {useHistory, useLocation} from 'react-router-dom'
 import {useTranslation} from 'react-i18next'
 
-import {classes as classesProps} from '../../props'
-import {DESERTS, DISH, STARTERS} from 'common/constants/recipe_types'
+import {classes as classesProps} from 'common/props'
+import {DESERTS, DISH, STARTERS} from 'common/constants/resources'
+import {DESERTS as desertsId, DISH as dishId, STARTERS as startersId} from 'common/constants/recipe_types'
 import {getDesertsRoute, getDishRoute, getStartersRoute} from 'common/routing/routesResolver'
 
 function NavBar({classes}) {
   const {t} = useTranslation()
   const history = useHistory()
+  const location = useLocation()
+  const currentLocationName = location.pathname.replace('/', '')
 
   function handleDishPageDisplay() {
     return history.push(getDishRoute())
@@ -26,13 +29,31 @@ function NavBar({classes}) {
   return (
     <div>
       <Grid container justify="center">
-        <Button key={STARTERS} variant="contained" onClick={handleStartersPageDisplay} className={classes.button}>
+        <Button
+          key={startersId}
+          variant="contained"
+          onClick={handleStartersPageDisplay}
+          color="primary"
+          className={currentLocationName === STARTERS ? classes.currentButton : classes.button}
+        >
           {t('navBar.starters')}
         </Button>
-        <Button key={DISH} variant="contained" onClick={handleDishPageDisplay}>
+        <Button
+          key={dishId}
+          variant="contained"
+          onClick={handleDishPageDisplay}
+          color="primary"
+          className={currentLocationName === DISH ? classes.currentButton : classes.button}
+        >
           {t('navBar.dish')}
         </Button>
-        <Button key={DESERTS} variant="contained" onClick={handleDesertsPageDisplay}>
+        <Button
+          key={desertsId}
+          variant="contained"
+          onClick={handleDesertsPageDisplay}
+          color="primary"
+          className={currentLocationName === DESERTS ? classes.currentButton : classes.button}
+        >
           {t('navBar.deserts')}
         </Button>
       </Grid>
--- src/common/components/NavBar/styles.js
+++ src/common/components/NavBar/styles.js
@@ -1,5 +1,11 @@
 export default ({palette}) => ({
   button: {
-    color: palette.primary.main,
+    backgroundColor: palette.primary.main,
+    margin: '0.5rem 0.2rem',
+  },
+  currentButton: {
+    backgroundColor: palette.primary.medium,
+    color: palette.primary.light,
+    margin: '0.5rem 0.2rem',
   },
 })
